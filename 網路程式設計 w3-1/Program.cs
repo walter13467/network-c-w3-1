078c2d9e6b270f6106692c91289a126597504b0e
冠德 江 <walter13467@outlook.com>
2022-03-14 10:14:35 +0800

    加入專案檔案。

---
+++ 網路程式設計 w3-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n % 2 == 1)
+            {
+                Console.WriteLine("Weird");
+            }
+            Console.WriteLine();
+        }
+    }
+}
