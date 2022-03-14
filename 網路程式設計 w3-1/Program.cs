b7812f3e8122b81620ccad315d207455a7480b02
冠德 江 <walter13467@outlook.com>
2022-03-14 10:33:37 +0800

    3

--- 網路程式設計 w3-1/Program.cs
+++ 網路程式設計 w3-1/Program.cs
@@ -9,6 +9,8 @@ namespace program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("輸入數字:");
+
             int n = Convert.ToInt32(Console.ReadLine());
 
             if (n % 2 == 1)
